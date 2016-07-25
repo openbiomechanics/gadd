fc64e6ec18ef323c051bc8441e90da553477b64d
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-07-24 20:16:22 -0500

    cleaned up munge script

--- munge/01-combine.py
+++ munge/01-combine.py
@@ -16,43 +16,35 @@ def main():
 
     # Load data
     data_dir = os.path.abspath('../data/original')
-    data_dir = os.path.abspath('./data/original')
     zipfile_name = 'gait-data.tar.gz'
     file = os.path.join(data_dir, zipfile_name)
     data = pd.DataFrame()
-    tar = tarfile.open(file)
-    for member in tar.getmembers():
-        if '.txt' in member.name:
-            f = tar.extractfile(member)
-            content = StringIO(f.read().decode('utf-8'))
-            df = pd.read_csv(content, sep="\t",
-                             names = ['time', 'stride_interval'])
-            name = member.name
-            name = name.replace('gait-data/', '')
-            name = name.replace('-si.txt', '')
-            print(name)
-            if '-' in name:
-                trial = name[0]
-                participant = name[1]
-                age = int(name[3:6])
-            else:
-                trial = name[0:2]
-                participant = name[2]
-                age = None
+    with tarfile.open(file) as tar:
+        for member in tar.getmembers():
+            if '.txt' in member.name:
+                f = tar.extractfile(member)
+                content = StringIO(f.read().decode('utf-8'))
+                df = pd.read_csv(content, sep="\t",
+                                 names=['time', 'stride_interval'])
+                name = member.name
+                name = name.replace('gait-data/', '')
+                name = name.replace('-si.txt', '')
+                if '-' in name:
+                    trial = name[0]
+                    participant = name[1]
+                    age = int(name[3:6])
+                else:
+                    trial = name[0:2]
+                    participant = name[2]
+                    age = None
 
-
-            df['trial'] = trial
-            df['participant'] = participant
-            df['age'] = age
-            print(df.head())
-            data = pd.concat([data, df], axis=0)
-
-    tar.close()
-
-    # Create new features
+                df['trial'] = trial
+                df['participant'] = participant
+                df['age'] = age
+                data = pd.concat([data, df], axis=0)
 
     # Write out data
-    data.to_csv('./data/prepped/intervals.csv', index=False)
+    data.to_csv('../data/prepped/intervals.csv', index=False)
     print("Finished!")
 
 
